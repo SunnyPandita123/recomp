e0027db2aee6b30e0d1e715bf0332556171ac2cf
SunnyPandita123 <pandita.sunny123@gmail.com>
2023-05-08 17:01:15 +0530

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+function MyComp() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/React.createElement("header", {
+    className: "App-header"
+  }, /*#__PURE__*/React.createElement("p", null, "Edit ", /*#__PURE__*/React.createElement("code", null, "src/App.js"), " and save to reload."), /*#__PURE__*/React.createElement("a", {
+    className: "App-link",
+    href: "https://reactjs.org",
+    target: "_blank",
+    rel: "noopener noreferrer"
+  }, "Learn React")));
+}
+var _default = MyComp;
+exports.default = _default;
