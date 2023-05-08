efed8a511da5637000c6765bdd6db00063fe856e
SunnyPandita123 <pandita.sunny123@gmail.com>
2023-05-08 17:42:28 +0530

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function MyComp() {
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/_react.default.createElement("div", {
     className: "App"
-  }, /*#__PURE__*/React.createElement("header", {
+  }, /*#__PURE__*/_react.default.createElement("header", {
     className: "App-header"
-  }, /*#__PURE__*/React.createElement("p", null, "Edit ", /*#__PURE__*/React.createElement("code", null, "src/App.js"), " and save to reload."), /*#__PURE__*/React.createElement("a", {
+  }, /*#__PURE__*/_react.default.createElement("p", null, "Edit ", /*#__PURE__*/_react.default.createElement("code", null, "src/App.js"), " and save to reload."), /*#__PURE__*/_react.default.createElement("a", {
     className: "App-link",
     href: "https://reactjs.org",
     target: "_blank",
